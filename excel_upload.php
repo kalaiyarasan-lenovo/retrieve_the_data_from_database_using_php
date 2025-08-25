26df08a780ab46b5d2b2b0a91fd90a4eb2dc93bd
kalaiyarasan <kalaivelliangiri@gmail.com>
2025-08-25 10:35:08 +0530

    Update excel_upload.php

--- excel_upload.php
+++ excel_upload.php
@@ -1,13 +1,12 @@
 <?php
 session_start();
-
-// Database connection
 $host = "localhost";
 $user = "root";
 $pass = "";
 $db   = "questionsdb";
 
 $con = mysqli_connect($host, $user, $pass, $db);
+
 if(!$con){
     die("Database connection failed: " . mysqli_connect_error());
 }
@@ -30,51 +29,33 @@ if(isset($_POST['submit']))
         $count = 0;
         foreach($data as $row)
         {
-            if($count > 0) // Skip header row
+            if($count > 0) // Skip header
             {
-                $q_no                  = mysqli_real_escape_string($con, $row[0]);
-                $English_Q             = mysqli_real_escape_string($con, $row[1]);
-                $E_List_1              = mysqli_real_escape_string($con, $row[2]);
-                $E_List_2              = mysqli_real_escape_string($con, $row[3]);
-                $E_items               = mysqli_real_escape_string($con, $row[4]);
-                $E_optA                = mysqli_real_escape_string($con, $row[5]);
-                $E_optB                = mysqli_real_escape_string($con, $row[6]);
-                $E_optC                = mysqli_real_escape_string($con, $row[7]);
-                $E_optD                = mysqli_real_escape_string($con, $row[8]);
+                $q_no             = mysqli_real_escape_string($con, $row[0]);
+                $English_Q        = mysqli_real_escape_string($con, $row[1]);
+                $E_List_1         = mysqli_real_escape_string($con, $row[2]);
+                $E_List_2         = mysqli_real_escape_string($con, $row[3]);
+                $E_options        = mysqli_real_escape_string($con, $row[4]);
+                $E_correct_option = mysqli_real_escape_string($con, $row[5]);
+                $Tamil_Q          = mysqli_real_escape_string($con, $row[6]);
+                $T_list_1         = mysqli_real_escape_string($con, $row[7]);
+                $T_list_2         = mysqli_real_escape_string($con, $row[8]);
+                $T_options        = mysqli_real_escape_string($con, $row[9]);
+                $T_correct_option = mysqli_real_escape_string($con, $row[10]);
 
-                $Tamil_Q               = mysqli_real_escape_string($con, $row[9]);
-                $T_list_1              = mysqli_real_escape_string($con, $row[10]);
-                $T_list_2              = mysqli_real_escape_string($con, $row[11]);
-                $T_items               = mysqli_real_escape_string($con, $row[12]);
-                $T_optA                = mysqli_real_escape_string($con, $row[13]);
-                $T_optB                = mysqli_real_escape_string($con, $row[14]);
-                $T_optC                = mysqli_real_escape_string($con, $row[15]);
-                $T_optD                = mysqli_real_escape_string($con, $row[16]);
-
-                $E_options_json        = mysqli_real_escape_string($con, $row[17]);
-                $T_options_json        = mysqli_real_escape_string($con, $row[18]);
-                $answer_key            = mysqli_real_escape_string($con, $row[19]);
-                $correct_answer_text_en= mysqli_real_escape_string($con, $row[20]);
-
-                $query = "INSERT INTO mcq_questions 
-                (q_no, question_en, statement1_en, statement2_en, items_en, 
-                 option_a_en, option_b_en, option_c_en, option_d_en,
-                 question_ta, statement1_ta, statement2_ta, items_ta,
-                 option_a_ta, option_b_ta, option_c_ta, option_d_ta,
-                 options_en_json, options_ta_json, answer_key, correct_answer_text_en)
-                VALUES 
-                ('$q_no', '$English_Q', '$E_List_1', '$E_List_2', '$E_items',
-                 '$E_optA', '$E_optB', '$E_optC', '$E_optD',
-                 '$Tamil_Q', '$T_list_1', '$T_list_2', '$T_items',
-                 '$T_optA', '$T_optB', '$T_optC', '$T_optD',
-                 '$E_options_json', '$T_options_json', '$answer_key', '$correct_answer_text_en')";
+                $query = "INSERT INTO questions 
+                    (q_no, English_Q, E_List_1, E_List_2, E_options, E_correct_option, 
+                    Tamil_Q, T_list_1, T_list_2, T_options, T_correct_option)
+                    VALUES 
+                    ('$q_no', '$English_Q', '$E_List_1', '$E_List_2', '$E_options', '$E_correct_option',
+                     '$Tamil_Q', '$T_list_1', '$T_list_2', '$T_options', '$T_correct_option')";
 
                 mysqli_query($con, $query);
                 $msg = true;
             }
             else
             {
-                $count = 1; // mark header processed
+                $count = 1; 
             }
         }
 
